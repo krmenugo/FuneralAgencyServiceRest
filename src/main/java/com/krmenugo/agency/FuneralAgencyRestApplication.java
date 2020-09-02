bd62dfc7d6f66cb9dfed6577ac74bc0ca93675a7
krmenugo <krmenugo@hotmail.com>
2020-09-01 20:41:12 -0500

    Commit Main Class
    Clase principal del proyecto

---
+++ src/main/java/com/krmenugo/agency/FuneralAgencyRestApplication.java
@@ -0,0 +1,13 @@
+package com.krmenugo.agency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuneralAgencyRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuneralAgencyRestApplication.class, args);
+	}
+
+}
